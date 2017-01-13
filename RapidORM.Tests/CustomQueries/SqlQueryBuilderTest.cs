b4dfe2c92f8d4903ee1e66f496d8e08906fed945
Kent Aguilar <kentaguilar@gmail.com>
2017-01-13 17:18:17 +0800

    Test routine creatd for SQL Query Builder

--- RapidORM.Tests/CustomQueries/SqlQueryBuilderTest.cs
+++ RapidORM.Tests/CustomQueries/SqlQueryBuilderTest.cs
@@ -1,11 +1,38 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RapidORM.Data;
+using RapidORM.Client.SQL;
+using RapidORM.Interfaces;
+using RapidORM.Helpers;
+using RapidORM.Tests.Core;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 
 namespace RapidORM.Tests.CustomQueries
 {
     [TestClass]
     public class SqlQueryBuilderTest
     {
-        
+        public IQueryBuilder queryBuilder;
+
+        public SqlQueryBuilderTest()
+        {
+            Database.MySqlDb();
+            queryBuilder = new SqlQueryBuilder();
+        }
+
+        [TestMethod]
+        public void QueryBuilderSqlExecuteNonQueryTest()
+        {
+            string sql = "insert into Car(Name,Power,Cost) VALUES(@name,@power,@cost)";
+            queryBuilder.ExecuteNonQuery(sql, new[]{
+                new SqlParameter{ ParameterName="name", Value = "Ferrari" },
+                new SqlParameter{ ParameterName="power", Value = 10 },
+                new SqlParameter{ ParameterName="cost", Value = 100 },
+            });
+
+            Assert.Inconclusive("Car Inserted");
+        }
     }
 }
