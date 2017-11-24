4a1b0b7862ca35e8a6ca44bd7e3ed09bf7eee03a
Kent Aguilar <kentaguilar@gmail.com>
2017-11-24 23:57:38 +0800

    Added datetime helper method info and unit test

---
+++ RapidORM.Tests/Tests/Helpers/DateTimeHelperTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using RapidORM.Helpers;
+
+namespace RapidORM.Tests.Tests.Helpers
+{
+    [TestClass]
+    public class DateTimeHelperTest
+    {
+        [TestMethod]
+        public void GetDateTimeForDBTest()
+        {
+            Assert.AreEqual("test", DateHelper.GetDateTimeForDB());
+        }
+    }
+}
--- RapidORM/Helpers/DateHelper.cs
+++ RapidORM/Helpers/DateHelper.cs
@@ -9,16 +9,28 @@ namespace RapidORM.Helpers
 {
     public class DateHelper
     {
+        /// <summary>
+        /// Converts normal datetime value to a formatted one
+        /// </summary>
+        /// <param name="givenDate"></param>
         public static string GetFormattedDateFromRawDate(DateTime givenDate)
         {
             return givenDate.Year.ToString() + givenDate.Month.ToString("d2") + givenDate.Day.ToString("d2");
         }
 
+        /// <summary>
+        /// Get current time that is compatible with database datetime type
+        /// </summary>
+        /// <param name=""></param>
         public static string GetDateTimeForDB()
         {
             return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
         }
 
+        /// <summary>
+        /// Check if given raw date is valid date
+        /// </summary>
+        /// <param name="value"></param>
         public static bool IsValidDate(string value)
         {
             string format = "MM/dd/yy";
@@ -34,6 +46,10 @@ namespace RapidORM.Helpers
             }
         }
 
+        /// <summary>
+        /// Converts date in string format to datetime format
+        /// </summary>
+        /// <param name="value"></param>
         public static DateTime ConvertDateStringToDateTime(string value)
         {
             return DateTime.ParseExact(value, "MM/dd/yyyy", CultureInfo.InvariantCulture);
